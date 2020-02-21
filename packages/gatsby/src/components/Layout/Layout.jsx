17f7979fd51d3604c75eef0b3ba04f1518b1ae82
bumpewto <t.boumpoutou@gmail.com>
2020-02-21 16:14:05 +0100

    Add closing on outside click to Layout

--- packages/gatsby/src/components/Layout/Layout.jsx
+++ packages/gatsby/src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 import styled from "styled-components";
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import { GlobalStyle } from "../../globals";
 import { Navbar } from "./Navbar";
 import { MenuPanel } from "./MenuPanel";
+import { useOnClickOutside } from "./hook";
 
 const Div = styled.div`
   margin: 100px 0 auto auto;
@@ -15,6 +16,8 @@ const Div = styled.div`
 
 export const Layout = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setMenuOpen(false));
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -28,12 +31,14 @@ export const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
-      <Navbar
-        siteTitle={data.site.siteMetadata.title}
-        menuOpen={menuOpen}
-        setMenuOpen={setMenuOpen}
-      />
-      <MenuPanel menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div ref={node}>
+        <Navbar
+          siteTitle={data.site.siteMetadata.title}
+          menuOpen={menuOpen}
+          setMenuOpen={setMenuOpen}
+        />
+        <MenuPanel menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
       <Div>
         <main>{children}</main>
         <footer>© {new Date().getFullYear()} g.z. studio</footer>
