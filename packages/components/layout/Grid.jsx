ad6decc4b9a1b2c1b84f776f6131a3efb20e65b2
bumpewto <t.boumpoutou@gmail.com>
2020-05-07 15:17:35 +0200

    Refactor Grid component

--- packages/components/layout/Grid.jsx
+++ packages/components/layout/Grid.jsx
@@ -1,44 +1,30 @@
-import React from "react";
-import { css } from "styled-components";
+import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 import { propertyExistOrVoid, CloneOrDiv } from "../utils";
 import { spacingCases } from "./spacingCases";
 
 // on applique le format de grid à condition que la prop correspondante existe dans le composant Grid
 
-const columnSelector = (columnProp) =>
-  propertyExistOrVoid(columnProp)(`grid-template-columns: ${columnProp};`);
-const rowSelector = (rowProp) =>
-  propertyExistOrVoid(rowProp)(`grid-template-rows: ${rowProp};`);
+// const columnSelector = (columnProp) =>
+//   propertyExistOrVoid(columnProp)(`grid-template-columns: ${columnProp};`);
+// const rowSelector = (rowProp) =>
+//   propertyExistOrVoid(rowProp)(`grid-template-rows: ${rowProp};`);
 
 // styles à injecter
 const gridStyle = css`
   display: grid;
-  ${(props) => columnSelector(props.columns)}
-  ${(props) => rowSelector(props.rows)}
-  gap: ${(props) => spacingCases(props.gap)}rem;
+  ${({ columns }) => `grid-template-columns: ${columns || `100%`};`}
+  ${({ rows }) => `grid-template-rows: ${rows || `100%`};`}
+  gap: ${({ gap }) => spacingCases(gap)}rem;
   grid-auto-flow: row;
   align-items: center;
   justify-content: center;
+  width: 100%;
 `;
 
-export const Grid = ({ columns, rows, gap, className, children }) => (
-  <CloneOrDiv
-    columns={columns}
-    rows={rows}
-    gap={gap}
-    children={children}
-    cloneStyle={gridStyle}
-    className={className}
-  />
-);
-
-Grid.propTypes = {
-  columns: PropTypes.string,
-  rows: PropTypes.string,
-  gap: PropTypes.string,
-  children: PropTypes.node,
-};
+export const Grid = styled(CloneOrDiv)`
+  ${gridStyle}
+`;
 
 // @@@@@@@@@ Old Grid with local CloneOrDiv component
 
